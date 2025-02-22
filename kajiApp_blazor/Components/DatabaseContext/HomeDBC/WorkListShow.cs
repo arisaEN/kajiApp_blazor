2a8cd20179e1330ea5b4a27b0bbd1c70c590838f
arisaEN <hkyamaha234@gmail.com>
2025-02-22 14:12:39 +0900

    Eatコンポーネント化前

--- kajiApp_blazor/Components/DatabaseContext/HomeDBC/WorkListShow.cs
+++ kajiApp_blazor/Components/DatabaseContext/HomeDBC/WorkListShow.cs
@@ -10,7 +10,7 @@ namespace kajiApp_blazor.Components.DatabaseContext.HomeDBC
         //非同期版
         public  async Task<List<WorkList>> GetWorksAsync() // ✅ 非同期メソッド
         {
-            //await Task.Delay(1000);
+            await Task.Delay(1000);
             using var connection = new SqliteConnection(_connectionString);
             await connection.OpenAsync(); // ✅ OpenAsync() を使う
 
@@ -39,35 +39,4 @@ namespace kajiApp_blazor.Components.DatabaseContext.HomeDBC
     }
 }
 
-
-//public class DBContextSqlite
-//{
-//    private const string _connectionString = "Data Source=database.db";
-
-//    public List<T> GetDB(string query)
-//    {
-//        await Task.Delay(2000);
-//        using var connection = new SqliteConnection(_connectionString);
-//        await connection.OpenAsync();
-
-//        var command = connection.CreateCommand();
-//        command.CommandText = query;
-
-//        var works = new List<T>();
-//        using var reader = await command.ExecuteReaderAsync();
-//        while (await reader.ReadAsync()) 
-//        {
-//            works.Add(new T
-//            {
-//                Id = reader.GetInt32(0),
-//                Day = reader.GetDateTime(1),
-//                Name = reader.GetString(2),
-//                WorkName = reader.GetString(3),
-//                Percent = reader.GetDouble(4)
-//            });
-//        }
-//        return works;
-//    }
-
-//}
 
