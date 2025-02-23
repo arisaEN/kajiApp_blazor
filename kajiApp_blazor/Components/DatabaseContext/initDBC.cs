e847fcc568e4211e37ab76ad61aef36d79cd19b9
arisaEN <hkyamaha234@gmail.com>
2025-02-23 10:28:41 +0900

    option完成2

---
+++ kajiApp_blazor/Components/DatabaseContext/initDBC.cs
@@ -0,0 +1,19 @@
+﻿namespace kajiApp_blazor.Components.DatabaseContext
+{
+    /// <summary>
+    /// DBの初期化処理
+    /// </summary>
+    public class initDBC
+    {
+       //今月のレコードを挿入管理者画面支払い情報のチェック
+
+
+        //今月のレコードを挿入 eat食費
+    
+        //今月のレコードを挿入 life生活費
+
+
+
+
+    }
+}
