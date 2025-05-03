df7c6d69634c3133dc1a50f9350a56c1ee2d245d
arisaEN <hkyamaha234@gmail.com>
2025-05-03 19:40:19 +0900

    jsでブラウザにcookie保存させる仕組みを作った

---
+++ kajiApp_blazor/wwwroot/js/cookieHelper.js
@@ -0,0 +1,24 @@
+// cookieHelper.js
+window.cookieHelper = {
+    setCookie: function (name, value, days) {
+        const d = new Date();
+        d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + d.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+    },
+    getCookie: function (name) {
+        const cname = name + "=";
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(cname) === 0) {
+                return c.substring(cname.length, c.length);
+            }
+        }
+        return "";
+    }
+};
